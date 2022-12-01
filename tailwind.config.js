77a761322490ec51e1e684667e19029cf383de9c
Patricio Lopez J <lopezjuripatricio@gmail.com>
2022-12-01 09:25:19 -0300

    fix: Fix next-themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // for next-themes
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
